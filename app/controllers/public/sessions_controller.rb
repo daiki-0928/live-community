ea39262a773009479966c5931da1be10da2b6e9b
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-06-17 09:21:37 +0000

    [Add]退会機能

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,4 +24,18 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  protected
+   # 退会しているかを判断するメソッド
+  def customer_state
+  ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @user = User.find_by(email: params[:customer][:email])
+  ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@user
+  ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @user.valid_password?(params[:customer][:password]) && (@user.is_active == false)
+      redirect_to new_customer_registration_path
+    ## 【処理内容3】
+    end
+  end
 end
