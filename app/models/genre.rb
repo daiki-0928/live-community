b34da75fbf07e2d066268a4443c4bebe7e78b2ea
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-06-18 07:32:15 +0000

    [Add]adminジャンル機能

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,6 @@
 class Genre < ApplicationRecord
+
+  has_many :posts, dependent: :destroy
+
+  validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,9 @@ devise_for :users, controllers: {
 devise_for :admin, controllers: {
   sessions: "admin/sessions"
 }
+
+namespace :admin do
+  resources :genres, only: [:index, :create, :edit, :update, :destroy]
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
