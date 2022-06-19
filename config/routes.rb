8185c12a4568626fb010aab07575041bf953c793
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-06-19 07:14:58 +0000

    [Add]admin投稿コメント

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  namespace :public do
+    get 'homes/top'
+  end
+devise_scope :user do
+    post 'users/sign_up' => 'public/registrations#create'
+    post 'users/guest_sign_in' => 'public/sessions#guest_sign_in'
+ end
+
 # 顧客用
 # URL /users/sign_in ...
 devise_for :users, controllers: {
@@ -15,7 +24,23 @@ devise_for :admin, controllers: {
 namespace :admin do
   resources :genres, only: [:index, :create, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update, :destroy]
-  resources :posts, only: [:index, :show, :edit, :update, :destroy]
-end
+  resources :posts, only: [:index, :show, :edit, :update, :destroy] do
+   resources :post_comments, only: [:destroy]
+   end
+ end
+
+ scope module: "public" do
+      root to: "homes#top"
+      get "/users/my_page" => "users#show"
+      get "/users/:id/confirm" => "users#confirm", as: "confirm"
+      patch "/users/:id/withdraw" => "users#withdraw", as: "withdarw"
+      get "/genre/:id" => "genres#show"
+      resources :users, only: [:index, :show, :edit, :update]
+      resources :posts, only: [:index, :show, :edit, :update, :destroy] do
+        resources :post_comments, only: [:create, :destroy]
+        resource :favorites, only: [:create, :destroy]
+      end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
