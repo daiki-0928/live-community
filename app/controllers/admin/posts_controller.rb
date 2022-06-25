63d3b1851256f76635f46b315bc9546a4a0a7228
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-06-25 09:55:15 +0000

    [Add]admin検索機能

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -7,7 +7,6 @@ class Admin::PostsController < ApplicationController
   end
 
   def show
-   @user = User.find(params[:id])
    @post = Post.find(params[:id])
    @post_comments = @post.post_comments
   end
--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -27,6 +27,6 @@ class Admin::SessionsController < Devise::SessionsController
 
   protected
   def after_sign_in_path_for(resource)
-    admin_genres_path
+    admin_users_path
   end
 end
