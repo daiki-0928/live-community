9aaec3b8a2d5492532978298c2f9670e5829d621
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-06-25 06:52:04 +0000

    [Add]public投稿検索

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,12 @@ class Post < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
+
+  def self.search(keyword)
+    if keyword
+      Post.where(['name LIKE?', "%#{keyword}%"])
+    else
+      Post.all
+    end
+  end
 end
